2a13983da62a8bc73d589c66bedea7ddcbf1a5b7
Nitin Kanade <ask.ni18.in@gmail.com>
2025-02-09 14:09:36 +0530

    v0.4

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
+const isProduction = process.env.NODE_ENV === 'production';
+
 module.exports = {
+    basePath: isProduction ? '/blog' : '', // Use '/blog' only in production
     output: 'export', // Enable static export
     distDir: 'dist',
     images: {
